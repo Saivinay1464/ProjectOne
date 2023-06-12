35601e54a267cc10f6fdc55a03ed66b39775d54b
Saivinay GitHub <p.saivinay98@gmail.com>
2023-06-12 16:37:35 -0500

    THird Spring Commit

--- src/main/java/com/example/ProjectOne/Controller/StudentController.java
+++ src/main/java/com/example/ProjectOne/Controller/StudentController.java
@@ -2,19 +2,31 @@ package com.example.ProjectOne.Controller;
 
 import com.example.ProjectOne.Model.Students;
 import com.example.ProjectOne.Service.StudentsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+
 @RequestMapping ("/Student")
+
 public class StudentController {
+    private static Logger log = LoggerFactory.getLogger(StudentController.class);
+
+
     @Autowired
     StudentsService Sservice;
-
+    @Operation(description = "This is the Student Details list")
+    @ApiResponse(responseCode = "200", description = "All the details of Students is fetched")
+    @ApiResponse(responseCode = "400", description = "Unable to fetch the details of Students")
     @GetMapping("/Details")
     public List<Students> studentdetails(){
+        log.info("Method for getting student details");
         return Sservice.studentdetails();
     }
 
